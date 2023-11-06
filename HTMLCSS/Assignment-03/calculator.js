eb47dfdb1c90cfbd43e37add57a80bf3659b23a6
N100lanki <theninjan.com@gmail.com>
2023-11-06 18:25:13 +0530

    Assignment-03 done

---
+++ HTMLCSS/Assignment-03/calculator.js
@@ -0,0 +1,35 @@
+
+//  Write a function to calculate which can add, subtract, multiply and divide two numbers. Use switch inside function.
+function calculate(operation, num1, num2) {
+    let result;
+  
+    switch (operation) {
+      case 'add':
+        result = num1 + num2;
+        break;
+      case 'subtract':
+        result = num1 - num2;
+        break;
+      case 'multiply':
+        result = num1 * num2;
+        break;
+      case 'divide':
+        if (num2 !== 0) {
+          result = num1 / num2;
+        } else {
+          result = 'Division by zero is not allowed';
+        }
+        break;
+      default:
+        result = 'Invalid operation';
+    }
+  
+    return result;
+  }
+  console.log(calculate('add', 5, 3));        
+  console.log(calculate('subtract', 10, 4));   
+  console.log(calculate('multiply', 6, 2));    
+  console.log(calculate('divide', 8, 2));   
+  console.log(calculate('divide', 5, 0));      
+  console.log(calculate('unknown', 2, 3));    
+  
---
+++ HTMLCSS/Assignment-03/even_0_to_100.js
@@ -0,0 +1,6 @@
+// even number zero to hundred
+
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+  }
+  
