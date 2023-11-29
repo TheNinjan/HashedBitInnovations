62ab3371905b1f29aac52eaa61fb932fb66141bb
N100lanki <theninjan.com@gmail.com>
2023-11-29 10:34:59 +0530

    Assignment05:Completed

--- HTMLCSS/Assignment-04/5_replace.js
+++ HTMLCSS/Assignment-04/5_replace.js
@@ -2,3 +2,13 @@
 Like this - correctfn(string, wrong, correct)
 Use string.replace in function.
 */
+
+function replaceWrongWithCorrect(sentence, wrong, correct) {
+    const correctedSentence = sentence.replace(wrong,correct);
+    return correctedSentence;
+  }  
+  const inputSentence = "This is a test sentence with wrong word.";
+  const wrongWord = "wrong";
+  const correctWord = "correct";
+  const correctedSentence = replaceWrongWithCorrect(inputSentence, wrongWord, correctWord);
+  console.log(correctedSentence);
